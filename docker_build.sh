bc8bd3a07d789a52cc4c821131f440d4c38037d9
I Gede Adi Martha Ardiana Putra <billytheburninginferno@gmail.com>
2022-07-24 16:25:34 +0800

    Add auto tag to docker build

--- docker_build.sh
+++ docker_build.sh
@@ -14,3 +14,18 @@ flutter build web --release -t lib/main.prod.dart
 
 # build the docker based on the build
 docker build -t adimartha/my_expense .
+
+# once finished build then get the current tag from the environment file
+tag=`cat conf/.prod.env | sed '2q;d' | awk -F "=" '{print $2}' | sed "s/['\"]//g" | awk -F "-" '{print $1}'`
+echo current tag is $tag
+
+# then tag the latest docker image to the current tag
+echo tag latest image to $tag
+docker image tag adimartha/my_expense:latest adimartha/my_expense:$tag
+
+# push both of the image to the docker repo
+echo push latest docker image
+docker image push adimartha/my_expense:latest
+
+echo push $tag docker image
+docker image push adimartha/my_expense:$tag
