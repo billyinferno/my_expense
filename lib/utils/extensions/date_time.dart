9131b9b33e2081e461200c3a366bc1661dd74fe7
billyinferno <billytheburninginferno@gmail.com>
2024-08-30 12:30:17 +0800

    Correct the comparison between the date on the date time extension

--- lib/utils/extensions/date_time.dart
+++ lib/utils/extensions/date_time.dart
@@ -5,9 +5,9 @@ extension CustomDateExtestion on DateTime {
     // check if year same or not?
     if (year == date.year) {
       // same, now check if month is the same or not?
-      if (month == month) {
+      if (month == date.month) {
         // same month, now check if the day is the same or not?
-        if (day == day) {
+        if (day == date.day) {
           // same day, so this is same day
           return true;
         }
