46f1125fe0e8902370e1e184ca7167d541ef4c1f
I Gede Adi Martha <billytheburninginferno@gmail.com>
2024-09-23 19:13:14 +0800

    Refactor MySwitch so it will showed accentColors[0] instead of [6] to align with other CupertinoSwitch activeTrackColor.

--- lib/widgets/input/switch.dart
+++ lib/widgets/input/switch.dart
@@ -3,33 +3,42 @@ import 'package:my_expense/_index.g.dart';
 
 class MySwitch extends StatelessWidget {
   final bool enabled;
-  final double? width;
+  final double width;
   final Color? activeColor;
-  final Color? disabledColor;
-  final Color? borderColor;
-  final Color? toggleColor;
+  final Color disabledColor;
+  final Color borderColor;
+  final Color toggleColor;
 
-  const MySwitch({ super.key, required this.enabled, this.width, this.activeColor, this.disabledColor, this.borderColor, this.toggleColor});
+  const MySwitch({
+    super.key,
+    required this.enabled,
+    this.width = 40,
+    this.activeColor,
+    this.disabledColor = secondaryDark,
+    this.borderColor = secondaryLight,
+    this.toggleColor = textColor2
+  });
 
   @override
   Widget build(BuildContext context) {
-    double currentWidth = (width ?? 40);
-    Color currentActiveColor = (activeColor ?? accentColors[6]);
-    Color currentDisabledColor = (disabledColor ?? secondaryDark);
-    Color currentBorderColor = (borderColor ?? secondaryLight);
+    Color currentActiveColor = (activeColor ?? accentColors[0]);
 
     return SizedBox(
       height: 25,
-      width: currentWidth,
+      width: width,
       child: Stack(
         children: <Widget>[
           Container(
             height: 25,
-            width: currentWidth,
+            width: width,
             decoration: BoxDecoration(
-              color: (enabled ? currentActiveColor : currentDisabledColor),
+              color: (enabled ? currentActiveColor : disabledColor),
               borderRadius: BorderRadius.circular(25),
-              border: Border.all(color: currentBorderColor, width: 1.0),
+              border: Border.all(
+                color: borderColor,
+                width: 1.0,
+                style: BorderStyle.solid,
+              ),
             ),
           ),
           Positioned(
@@ -41,7 +50,11 @@ class MySwitch extends StatelessWidget {
               decoration: BoxDecoration(
                 color: textColor2,
                 borderRadius: BorderRadius.circular(25),
-                border: Border.all(color: currentBorderColor, width: 1.0),
+                border: Border.all(
+                  color: borderColor,
+                  width: 1.0,
+                  style: BorderStyle.solid,
+                ),
               ),
             ),
           )
