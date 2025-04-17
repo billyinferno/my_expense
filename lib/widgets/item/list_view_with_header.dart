bf5d6c3cc3e0d3ac02c9fd3dfee036019181448c
I Gede Adi Martha <billytheburninginferno@gmail.com>
2025-04-17 08:59:34 +0800

    Implement the group function on the list view with header. Not tested yet, so need to check once we implement the group by selection on the transaction search page.

--- lib/widgets/item/list_view_with_header.dart
+++ lib/widgets/item/list_view_with_header.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'package:easy_sticky_header/easy_sticky_header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
@@ -55,120 +56,251 @@ class ListViewWithHeader extends StatelessWidget {
     }
   }
 
+  Map<String, List<TransactionListModel>> _groupData() {
+    final SplayTreeMap<String, List<TransactionListModel>> mapData = SplayTreeMap<String, List<TransactionListModel>>();
+    String mapKey;
+    
+    if (groupBy == GroupBy.none) {
+      mapData[""] = data;
+    }
+    else {
+      // let's generate the mapData based on the category
+      // the data is already sorted so what we need to do is just to add it
+      // on the correct category
+      
+      // loop on the data
+      for(int i=0; i<data.length; i++) {
+        if (data[i].type.toLowerCase() == 'transfer') {
+          // we will loop transfer into single category
+          if (!mapData.containsKey('transfer')) {
+            // no map data transfer, create a new one
+            mapData['transfer_transfer'] = [];
+          }
+
+          // add data to the map data
+          mapData['transfer_transfer']!.add(data[i]);
+        }
+        else {
+          // this is income and expense, for this we can store it based on the
+          // correct category for the transaction it self
+
+          // generate map key
+          mapKey = "${data[i].type.toLowerCase()}_${data[i].category!.name.toLowerCase()}";
+
+          // check if map key already exists or not?
+          if (!mapData.containsKey(mapKey)) {
+            // map key not exists, create a new list for this
+            mapData[mapKey] = [];
+          }
+
+          // add data to the map data
+          mapData[mapKey]!.add(data[i]);
+        }
+      }
+    }
+
+    return mapData;
+  }
+
   List<Widget> _generateChildWidget(BuildContext context) {
     final List<Widget> widgetList = [];
-    
-    // data is not empty, loop thru data
+    final Map<String, List<TransactionListModel>> mapData = _groupData();
+
     DateTime prevDate = DateTime.now();
     String prevName = '';
     double prevAmount = 0;
+    int index = 0;
+    int? parentIndex;
+    
+    // check if map data is empty or not?
+    if (mapData.isNotEmpty) {
+      // loop thru the map data
+      mapData.forEach((key, transactions) {
+        // check if key is not empty
+        if (key.isNotEmpty) {
+          // create header for the group
+          widgetList.add(_groupHeader(index: index, keys: key));
 
-    for(int i=0; i<data.length; i++) {
-      // check whether we want to show header or not?
-      if (showHeader) {
-        // first data, add the header nonetheless
-        if (i == 0) {
-          switch (headerType) {
-            case HeaderType.name:
-              prevName = data[i].name;
-              widgetList.add(
-                _header(
-                  text: data[i].name,
-                  index: i,
-                )
-              );
-              break;
-            case HeaderType.amount:
-              prevAmount = data[i].amount;
-              widgetList.add(
-                _header(
-                  text: data[i].amount.formatCurrency(shorten: false, decimalNum: 2),
-                  index: i,
-                )
-              );
-              break;
-            default:
-              prevDate = data[i].date;
-              widgetList.add(
-                _header(
-                  text: Globals.dfddMMMMyyyy.formatLocal(prevDate),
-                  index: i,
-                )
-              );
-              break;
-          }
-        }
-        else {
-          // check what kind of header type?
-          switch (headerType) {
-            case HeaderType.name:
-              // check whether prev name is same as current name or not?
-              if (prevName != data[i].name) {
-                // prevName is not same with current name, generate a header and set
-                // prevName as current name.
-                prevName = data[i].name;
-                widgetList.add(
-                  _header(
-                    text: data[i].name,
-                    index: i,
-                  )
-                );
-              }
-              break;
-            case HeaderType.amount:
-              // check whether prev amount is same as current amount or not?
-              if (prevAmount != data[i].amount) {
-                // prevAmount is not same with current amount, generate a header and set
-                // prevAmount as current amount.
-                prevAmount = data[i].amount;
-                widgetList.add(
-                  _header(
-                    text: data[i].amount.formatCurrency(shorten: false, decimalNum: 2),
-                    index: i,
-                  )
-                );
-              }
-              break;
-            default:
-              // check whether prev date is same as current date or not?
-              if (!prevDate.isSameDate(date: data[i].date)) {
-                // prevDate is not same with current date, generate a header and set
-                // prevDate as current date.
-                prevDate = data[i].date;
-                widgetList.add(
-                  _header(
-                    text: Globals.dfddMMMMyyyy.formatLocal(prevDate),
-                    index: i,
-                  )
-                );
-              }
-              break;
-          }
-        }
-      }
+          // set parent index as this index
+          parentIndex = index;
 
-      // other than that, we can just create the item based on the transaction
-      // data given.
-      widgetList.add(
-        _createItem(
-          txn: data[i],
-          canEdit: (onEdit != null),
-          canDelete: (onDelete != null),
-          context: context,
-        )
-      );
+          // add index since we already use this index
+          index = index + 1;
+        }
+
+        // loop thru the transactions
+        for(int i=0; i<transactions.length; i++) {
+          // check whether we want to show header or not?
+          if (showHeader) {
+            // first data, add the header nonetheless
+            if (i == 0) {
+              switch (headerType) {
+                case HeaderType.name:
+                  prevName = transactions[i].name;
+                  widgetList.add(
+                    _header(
+                      text: transactions[i].name,
+                      index: index,
+                      parentIndex: parentIndex,
+                    )
+                  );
+                  break;
+                case HeaderType.amount:
+                  prevAmount = transactions[i].amount;
+                  widgetList.add(
+                    _header(
+                      text: transactions[i].amount.formatCurrency(
+                        shorten: false,
+                        decimalNum: 2
+                      ),
+                      index: index,
+                      parentIndex: parentIndex,
+                    )
+                  );
+                  break;
+                default:
+                  prevDate = transactions[i].date;
+                  widgetList.add(
+                    _header(
+                      text: Globals.dfddMMMMyyyy.formatLocal(prevDate),
+                      index: index,
+                      parentIndex: parentIndex,
+                    )
+                  );
+                  break;
+              }
+            }
+            else {
+              // check what kind of header type?
+              switch (headerType) {
+                case HeaderType.name:
+                  // check whether prev name is same as current name or not?
+                  if (prevName != transactions[i].name) {
+                    // prevName is not same with current name, generate a header
+                    // and set prevName as current name.
+                    prevName = transactions[i].name;
+                    widgetList.add(
+                      _header(
+                        text: transactions[i].name,
+                        index: index,
+                        parentIndex: parentIndex,
+                      )
+                    );
+                  }
+                  break;
+                case HeaderType.amount:
+                  // check whether prev amount is same as current amount or not?
+                  if (prevAmount != transactions[i].amount) {
+                    // prevAmount is not same with current amount, generate a
+                    // header and set prevAmount as current amount.
+                    prevAmount = transactions[i].amount;
+                    widgetList.add(
+                      _header(
+                        text: transactions[i].amount.formatCurrency(shorten: false, decimalNum: 2),
+                        index: index,
+                        parentIndex: parentIndex,
+                      )
+                    );
+                  }
+                  break;
+                default:
+                  // check whether prev date is same as current date or not?
+                  if (!prevDate.isSameDate(date: transactions[i].date)) {
+                    // prevDate is not same with current date, generate a header
+                    // and set prevDate as current date.
+                    prevDate = transactions[i].date;
+                    widgetList.add(
+                      _header(
+                        text: Globals.dfddMMMMyyyy.formatLocal(prevDate),
+                        index: index,
+                        parentIndex: parentIndex,
+                      )
+                    );
+                  }
+                  break;
+              }
+            }
+
+            // add index for header
+            index = index + 1;
+          }
+
+          // other than that, we can just create the item based on the
+          // transaction data given.
+          widgetList.add(
+            _createItem(
+              txn: data[i],
+              canEdit: (onEdit != null),
+              canDelete: (onDelete != null),
+              context: context,
+            )
+          );
+
+          // add index
+          index = index + 1;
+        }
+      },);
     }
 
     // return the widget list
     return widgetList;
   }
 
+  Widget _groupHeader({required String keys, required int index}) {
+    // split the "_" in the key
+    List<String> key = keys.split('_');
+    Color headerTextColor;
+
+    // check the first key whether this is income, expense, or transfer
+    // this will determine the color of the group text
+    switch(key[0]) {
+      case 'expense':
+        headerTextColor = lightAccentColors[2];
+        break;
+      case 'income':
+        headerTextColor = lightAccentColors[0];
+        break;
+      case 'transfer':
+        headerTextColor = lightAccentColors[4];
+        break;
+      default:
+        headerTextColor = textColor;
+        break;
+    }
+
+    return StickyContainerWidget(
+      index: index,
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+        decoration: BoxDecoration(
+          color: primaryDark,
+          border: Border(
+            bottom: BorderSide(
+              color: primaryLight,
+              width: 1.0,
+              style: BorderStyle.solid,
+            )
+          )
+        ),
+        child: Text(
+          key[1].toTitleCase(),
+          style: TextStyle(
+            color: headerTextColor,
+          ),
+        ),
+      ),
+    );
+  }
+
   Widget _header({
     required String text,
     required int index,
+    int? parentIndex,
   }) {
     return StickyContainerWidget(
       index: index,
+      parentIndex: parentIndex,
       child: Container(
         width: double.infinity,
         padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
